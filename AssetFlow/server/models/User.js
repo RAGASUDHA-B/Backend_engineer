82dc60793c6b8451a72859f761a0ecac85398da6
RAGASUDHA-B <sudha.balu.471@gmail.com>
2026-07-27 18:56:38 +0530

    organize backend engineer learning repository

---
+++ AssetFlow/server/models/User.js
@@ -0,0 +1,37 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["Admin","Employee","AssetManager","DepartmentHead"],
+        default:"Employee"
+    },
+    department:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "department"
+    },
+    phone:{
+        type:String
+    },
+    status:{
+        type:String,
+        enum:["Active","Inactive"],
+        default:"Active"
+    }
+
+},{timestamps:true});

05cfb7cdd5ca1fc1a58b6b076c2140e611c3dc59
RAGASUDHA-B <sudha.balu.471@gmail.com>
2026-07-27 10:04:50 +0530

    database connected

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+const connectDB=async ()=>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Mongodb Connected: ${conn.connection.host}`);
+    }catch(error){
+        console.error("Database Connection Failed");
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+module.exports=connectDB;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express=require("express");
+require("dotenv").congig();
+const connectDB=require("./config/db");
 const app=express()
-const PORT=5000;
+connectDB();
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Welcome to assetFlow backend!");
 });
+const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 });

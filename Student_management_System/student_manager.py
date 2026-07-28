4a51248c5fe4f77bbdc2ed8b336a3f89173f16e7
Hari Prasad <61858995+hari-prasad-1@users.noreply.github.com>
2026-07-28 15:10:09 +0530

    Enhance comments in StudentManager methods
    Added comments to clarify code functionality and improve readability.

--- Student_management_System/student_manager.py
+++ Student_management_System/student_manager.py
@@ -1,14 +1,14 @@
 class StudentManager:
     def __init__(self):
         self.students=[]
-    def add_student(self,student):
+    def add_student(self,student): # add type
         self.students.append(student)
         print("Student added successfully")
     def view_students(self):
         if len(self.students)==0:
             print("No students available")
             return
-        for student in self.students:
+        for student in self.students: 
             student.display()
     def search_student(self,student_id):
         for student in self.students:
@@ -17,6 +17,7 @@ class StudentManager:
                 return
         print("Studnet not found")
     def delete_student(self,student_id):
+        # use dict to avoid loops. keep student id as key
         for student in self.students:
             if student.student_id==student_id:
                 self.students.remove(student)
@@ -28,7 +29,7 @@ class StudentManager:
             print("No student")
             return
         topper=self.students[0]
-        for student in self.students:
+        for student in self.students: # try avoiding loops. it will take more time if data grows
             if student.calculate_average()>topper.calculate_average():
                 topper=student
         print("Top student:")

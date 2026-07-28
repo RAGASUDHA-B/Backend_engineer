a5ba81c50ea8e6314bc2aa634445ded4beb0c97c
Hari Prasad <61858995+hari-prasad-1@users.noreply.github.com>
2026-07-28 15:07:41 +0530

    Enhance Student class with comments and error handling
    Added comments for type checking and error handling.

--- Student_management_System/student.py
+++ Student_management_System/student.py
@@ -1,24 +1,25 @@
 class Student:
-    def __init__(self,student_id,name,age,marks):
+    # also read about dynamic args **kwargs
+    def __init__(self,student_id,name,age,marks): # its better to have type check like student_id: int, name: str like this it will avoid app crashes on wrong value
         self.student_id=student_id
         self.name=name
         self.age=age
         self.marks=marks
     def calculate_average(self):
         total=sum(self.marks)
-        average=total/len(self.marks)
+        average=total/len(self.marks) # it might throw div by zero error. handle that
         return average
-    def display(self):
+    def display(self): # read about __str__ 
         print("___________")
         print("ID:",self.student_id)
         print("Name: ",self.name)
         print("Age:",self.age)
         print("Marks:",self.marks)
         print("Average: ",self.calculate_average())
-    def to_dict(self):
+    def to_dict(self): # read about __dict__
         return {
         "id": self.student_id,
         "name": self.name,
         "age": self.age,
         "marks": self.marks
-    }
+    } # indendaiton error

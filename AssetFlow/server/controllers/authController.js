712c75d20417abc6a1cbd420680444acde7f1d00
RAGASUDHA-B <sudha.balu.471@gmail.com>
2026-07-30 01:07:40 +0530

    controling router and controllers to save data in js memory and mongodb

--- AssetFlow/server/controllers/authController.js
+++ AssetFlow/server/controllers/authController.js
@@ -1,10 +1,31 @@
-const User=require("../models/User");
-const registerUser=async(req,res)=>{
-    console.log(req.body);
-    res.json({
-        message:"Controller working"
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({   //400 bad request,200-success,201-created,401-unauthorized,404-notfound,500 server error
+            message:"Please fill all fields"
+        });
+    }
+    const existingUser=await User.findOne({email});  //find the user (findOne()) querying MongoDB  await=>asynchronous database operation
+    if(existingUser){
+        return res.status(400).json({
+            message:"User already exists"
+        });
+    }
+    const user=new User({
+        name,email,password
+    });
+    await user.save();
+
+    
+    res.status(201).json({
+        message:"User registered successfully",
+        user
     });
 };
-module.exports={
+
+module.exports = {
     registerUser
 };
--- AssetFlow/server/models/User.js
+++ AssetFlow/server/models/User.js
@@ -1,39 +1,9 @@
-const mongoose=require("mongoose");
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:["Admin","Employee","AssetManager","DepartmentHead"],
-        default:"Employee"
-    },
-    department:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: "department"
-    },
-    phone:{
-        type:String
-    },
-    status:{
-        type:String,
-        enum:["Active","Inactive"],
-        default:"Active"
-    }
+const mongoose = require("mongoose");
 
-},{timestamps:true});
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
 
-module.exports=mongoose.model("user",userSchema);
+module.exports = mongoose.model("User", userSchema);

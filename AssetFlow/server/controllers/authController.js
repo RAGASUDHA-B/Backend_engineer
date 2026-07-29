27ced6efdfa97327cbe10df10cfcdee36d173e00
RAGASUDHA-B <sudha.balu.471@gmail.com>
2026-07-29 19:38:46 +0530

    Complete register API

--- AssetFlow/server/controllers/authController.js
+++ AssetFlow/server/controllers/authController.js
@@ -1,23 +1,10 @@
 const User=require("../models/User");
 const registerUser=async(req,res)=>{
-    try{
-        const {name,email,password}=req.body;
-        const user=await User.create({
-            name,email,password
-        });
-        res.status(201).json({
-            success:true,
-            message:"User registered successfully",
-            user
-        });
-    }
-    catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
-        });
-    }
+    console.log(req.body);
+    res.json({
+        message:"Controller working"
+    });
 };
-
-module.exports={registerUser
+module.exports={
+    registerUser
 };

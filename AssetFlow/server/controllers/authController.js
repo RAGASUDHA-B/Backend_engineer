5da02089fc511a8709648b6ee49b884ae2b16aa8
RAGASUDHA-B <sudha.balu.471@gmail.com>
2026-07-30 08:30:11 +0530

    added login controller and login routes

--- AssetFlow/server/controllers/authController.js
+++ AssetFlow/server/controllers/authController.js
@@ -33,6 +33,41 @@ catch(error){
     });
 }
 };
+
+const loginUser=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                message:"Please provide email and password"
+            });
+
+        }
+        const user =await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid Email or Password"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Email or Password"
+            });
+        }
+
+        res.status(200).json({
+            message: "Login Successful"
+        });
+    }catch(error){
+        console.error(error);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    };
+};
 module.exports = {
-    registerUser
+    registerUser,loginUser
 };
